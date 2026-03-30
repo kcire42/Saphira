7cca5e266ad0fb02486321ac25670cb19ebaa2e2
Kcire_Server <erickcarrillo2807@gmail.com>
2026-03-30 06:02:07 +0000

    creaccion de query

--- ingest_service/app/Youtube_Extractor/Database/ingest_data.py
+++ ingest_service/app/Youtube_Extractor/Database/ingest_data.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from app.Youtube_Extractor.Database.config import Config
 import psycopg2
 
@@ -21,30 +22,31 @@ def register_youtube_ingestion(metadata):
     try:
         duration = int(metadata.get('duration', 0))
         views = int(metadata.get('view_count', 0))
+        published_at = datetime.strptime(metadata['upload_date'], '%Y-%m-%d')
         with conn.cursor() as cur:
             cur.execute("""
-                CALL "Youtube".register_youtube_ingestion(
+                CALL "youtube".register_youtube_ingestion(
                     p_video_id := %s::text,
-                    p_title := %s::text,
-                    p_url := %s::text,
-                    p_duration := %s::bigint,
-                    p_view_count := %s::bigint,
+                    p_video_url := %s::text,
+                    p_video_title := %s::text,
+                    p_video_duration := %s::bigint,
+                    p_video_view_count := %s::bigint,
                     p_channel_id := %s::text,
                     p_channel_name := %s::text,
                     p_channel_url := %s::text,
                     p_hash_video := %s::text,
-                    p_publish_at := %s::timestamp
+                    p_published_at := %s::timestamp
                         )""",(
                 str(metadata['video_id']),
-                str(metadata['title']),
                 str(metadata['url']),
+                str(metadata['title']),
                 duration,     # Enviado como int -> Postgres lo recibe como BIGINT
                 views,        # Enviado como int -> Postgres lo recibe como BIGINT
                 str(metadata['channel_id']),
                 str(metadata['channel_name']),
                 str(metadata['channel_url']),
                 str(metadata['video_hash']),
-                metadata['upload_date'] # '2024-01-01'
+                published_at # '2024-01-01'
             ))
         conn.commit()
         print(f"✅ Metadata del video '{metadata['title']}' ingresada correctamente.")
@@ -59,7 +61,7 @@ def download_youtube_ingestion(metadata):
     try:
         with conn.cursor() as cur:
             cur.execute("""CALL 
-                        "Youtube".download_youtube_ingestion(
+                        "youtube".download_youtube_ingestion(
                         p_video_id := %s::text
                         )""",(
                         video_id,
@@ -77,9 +79,9 @@ def process_youtube_ingestion(metadata, transcription):
     try:
         with conn.cursor() as cur:
             cur.execute("""CALL 
-                        "Youtube".process_youtube_ingestion(
+                        "youtube".process_youtube_ingestion(
                         p_video_id := %s::text,
-                        p_transcript := %s::text
+                        p_transcripcion := %s::text
                         )""",(
                         video_id, 
                         str(transcription)
@@ -90,13 +92,17 @@ def process_youtube_ingestion(metadata, transcription):
         print(f"❌ Error al procesar transcripción de videos: {e}")
         conn.rollback()
 
+# flow text:
+# pending -> downloaded -> processed -> summary -> vector -> learning -> completed 
+# error
+
 
 if __name__ == "__main__":
     # Ejemplo de uso
     sample_metadata = {
         'video_id': 'abc123',
-        'title': 'Ejemplo de Video2',
         'url': 'https://www.youtube.com/watch?v=abc1232',
+        'title': 'Ejemplo de Video2',     
         'duration': 300,
         'view_count': 1000,
         'channel_id': 'channel1232',
@@ -106,5 +112,6 @@ if __name__ == "__main__":
         'upload_date': '2024-01-02'
     }
     #register_youtube_ingestion(sample_metadata)
-    download_youtube_ingestion(sample_metadata)
-    #process_youtube_ingestion(sample_metadata, "Transcripción de ejemplo para el video.")
+    #download_youtube_ingestion(sample_metadata)
+    process_youtube_ingestion(sample_metadata, "Transcripción de ejemplo para el video.")
+
