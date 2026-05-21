9c5ca5a5c7eaeefb1c31fe75068e7264d57352bf
Kcire_Server <erickcarrillo2807@gmail.com>
2026-05-21 05:16:47 +0000

    cambiar chroma to qdrant

--- embedding_service/app/RAG/train.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-from embedding_service.app.RAG.trainData import trainRagData
-
-trainRouter = APIRouter(prefix='/train')
-
-@trainRouter.post('/')
-async def train_rag():
-    trainRagData()
-    return {"status": "RAG training completed."}
